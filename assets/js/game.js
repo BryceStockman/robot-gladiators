761694c38ed1d30afe0324f6256f56f09e86a924
BryceStockman <65779581+BryceStockman@users.noreply.github.com>
2021-11-05 20:49:56 -0600

    localStorage added for highscore

--- assets/js/game.js
+++ assets/js/game.js
@@ -169,12 +169,29 @@ var startGame = function () {
 // function to end the entire game
 var endGame = function () {
   window.alert("The game has now ended. Let's see how you did!");
-  // if player is still alive, player wins!
-  if (playerInfo.health > 0) {
-    window.alert('Great job, you survived the game! You know have a score');
-  } else {
-    window.alert("You've lost your robot in battle.");
+
+  // check localStorage for high score, if it's not there, use 0
+  var highScore = localStorage.getItem('highscore');
+  if (highScore === null) {
+    highScore = 0;
   }
+  // if player has more money than the high score, player has new high score!
+  if (playerInfo.money > highScore) {
+    localStorage.setItem('highscore', playerInfo.money);
+    localStorage.setItem('name', playerInfo.name);
+
+    alert(
+      playerInfo.name + ' now has new high score of ' + playerInfo.money + '!'
+    );
+  } else {
+    alert(
+      playerInfo.name +
+        ' did not beat the high score of ' +
+        highScore +
+        '. Maybe next time!'
+    );
+  }
+
   // ask player if they would like to play again
   var playAgainConfirm = window.confirm('Would you like to play again?');
 
@@ -276,6 +293,3 @@ var enemyInfo = [
 
 // call the start the game function when the page loads
 startGame();
-
-// call the shop function
-shop();
